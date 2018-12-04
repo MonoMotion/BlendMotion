862be85cd142d04f2e04f226d1ba70a656ed91e2
coord.e <me@coord-e.com>
2018-12-04 07:27:30 +0000

    Add: Add a panel to exeute oeprator

--- blendmotion/operators/__init__.py
+++ blendmotion/operators/__init__.py
@@ -1,7 +1,7 @@
 import bpy
 
 from .add_bones import AddBonesOperator, SelectAndAddBonesOperator
-from .export_animation import ExportAnimationOperator
+from .export_animation import ExportAnimationOperator, ExportAnimationPanel
 
 def menu_func(self, context):
     self.layout.separator()
@@ -15,9 +15,11 @@ def register():
     bpy.utils.register_class(SelectAndAddBonesOperator)
     bpy.utils.register_class(AddBonesOperator)
     bpy.utils.register_class(ExportAnimationOperator)
+    bpy.utils.register_class(ExportAnimationPanel)
 
 def unregister():
     bpy.types.INFO_MT_armature_add.remove(menu_func)
+    bpy.utils.unregister_class(ExportAnimationPanel)
     bpy.utils.unregister_class(ExportAnimationOperator)
     bpy.utils.unregister_class(SelectAndAddBonesOperator)
     bpy.utils.unregister_class(AddBonesOperator)
--- blendmotion/operators/export_animation.py
+++ blendmotion/operators/export_animation.py
@@ -1,6 +1,16 @@
 import bpy
 from blendmotion.core import export_animation
 
+class ExportAnimationPanel(bpy.types.Panel):
+    bl_idname = 'bm.panel.export_animation'
+    bl_label = 'BlendMotion Animation Export'
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = 'render'
+
+    def draw(self, context):
+        self.layout.operator(ExportAnimationOperator.bl_idname)
+
 class ExportAnimationOperator(bpy.types.Operator):
     bl_idname = "bm.export_animation"
     bl_label = "Export animation"
