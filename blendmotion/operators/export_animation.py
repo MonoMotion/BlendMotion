54cdc8cad080271fab72a56f5d512c60142b1378
coord.e <me@coord-e.com>
2018-12-07 05:28:58 +0000

    Change: Refacrtor: Move ImportAnimationOperator to separated file

--- blendmotion/operators/export_animation.py
+++ blendmotion/operators/export_animation.py
@@ -44,33 +44,8 @@ class ExportAnimationOperator(bpy.types.Operator):
         context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}
 
-class ImportAnimationOperator(bpy.types.Operator):
-    bl_idname = "bm.import_animation"
-    bl_label = "Import Animation"
-
-    filepath = bpy.props.StringProperty(name='file_path', subtype='FILE_PATH')
-
-    def execute(self, context):
-        if len(context.selected_objects) != 1:
-            return error_and_log(self, 'Single object must be selected')
-
-        obj = context.selected_objects[0]
-        try:
-            import_animation(obj, self.filepath)
-        except OperatorError as e:
-            e.report(self)
-            e.log()
-            return {'CANCELLED'}
-
-        return {'FINISHED'}
-
-    def invoke(self, context, event):
-        context.window_manager.fileselect_add(self)
-        return {'RUNNING_MODAL'}
-
 def register():
     bpy.utils.register_class(ExportAnimationOperator)
-    bpy.utils.register_class(ImportAnimationOperator)
     bpy.utils.register_class(ExportAnimationPanel)
     bpy.utils.register_class(ExportAnimationProps)
     bpy.types.Scene.export_animation_props = bpy.props.PointerProperty(type=ExportAnimationProps)
@@ -79,4 +54,3 @@ def unregister():
     bpy.utils.unregister_class(ExportAnimationProps)
     bpy.utils.unregister_class(ExportAnimationPanel)
     bpy.utils.unregister_class(ExportAnimationOperator)
-    bpy.utils.unregister_class(ImportAnimationOperator)
