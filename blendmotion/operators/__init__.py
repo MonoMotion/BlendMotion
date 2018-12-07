cb5203c3ec9c6d5f7bc18361f5a4b9612393dff3
coord.e <me@coord-e.com>
2018-12-07 09:52:52 +0000

    Delete: Delete MarkAsEffector operators

--- blendmotion/operators/__init__.py
+++ blendmotion/operators/__init__.py
@@ -1,16 +1,14 @@
 import bpy
 
-from . import add_bones, export_animation, import_animation, mark_as_effector
+from . import add_bones, export_animation, import_animation
 
 
 def register():
     add_bones.register()
     export_animation.register()
     import_animation.register()
-    mark_as_effector.register()
 
 def unregister():
     add_bones.unregister()
     export_animation.unregister()
     import_animation.unregister()
-    mark_as_effector.unregister()
